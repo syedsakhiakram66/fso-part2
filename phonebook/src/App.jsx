7c20c3e32ed059e2081dcfdfb3b51f7baee9ac50
Sakhi Akram <sakhiakram.github@outlook.com>
2025-11-20 09:38:29 +0500

    exercise 2.6 solved

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  function handleNameChange(event) {
+    setNewName(event.target.value)
+  }
+
+  function addName(event) {
+    event.preventDefault()
+    setPersons([...persons, { name: newName }])
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person, index) => (
+            <p key={index}>{person.name}</p>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
